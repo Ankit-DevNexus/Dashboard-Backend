5fb7bc1e6307cd9d1db60fbae086af194fef8d1d
Abdul Azeem <abdulazeem@devnexus.in>
2025-07-10 12:13:05 +0530

    create leads API for dynamic fields and authenticate route

--- models/LeadModel.js
+++ models/LeadModel.js
@@ -2,53 +2,25 @@
 import mongoose, { Schema } from "mongoose";
 
 const leadSchema = new mongoose.Schema({
-    // Fixed fields
-    date: { 
-        type: Date, 
-        default: Date.now 
-    },
-    name: {
-        type: String
-    },
-    email: { 
-        type: String 
-    },
-    phone: { 
-        type: String 
-    },
-    city: { 
-        type: String 
-    },
-    budget: { 
-        type: String 
-    },
-    requirement: { 
-        type: String 
-    },
-    status: { 
-        type: String 
-    },
-    remarks1: { 
-        type: String 
-    },
-    remarks2: { 
-        type: String 
-    },
-    source:{
-        type: String 
-    },
-    Campaign:{
-        type: String 
-    },
-    createdBy:{
-        type: String 
-    },
-    // Dynamic fields container
-    extraFields: {
-        type: Schema.Types.Mixed,
-        default: {}
-    }
-}, { timestamps: true });
+  date: { type: Date, default: Date.now },
+  name: { type: String },
+  email: { type: String },
+  phone: { type: String },
+  city: { type: String },
+  budget: { type: String },
+  requirement: { type: String },
+  status: { type: String },
+  remarks1: { type: String },
+  remarks2: { type: String },
+  source: { type: String },
+  Campaign: { type: String },
+  createdBy: { type: String }
+}, {
+  timestamps: true,
+  strict: false // allows dynamic fields to be stored as top-level fields (like as static fields)
+});
+
+
 
 const LeadsModel =  mongoose.model("Lead", leadSchema);
 export default LeadsModel;
